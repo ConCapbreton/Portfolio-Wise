222ffe6d25e7fb7aa673da1e9991d47171a347ee
Connor <connorsexton@hotmail.com>
2025-03-02 12:34:49 +0100

    createBrowserRouter replaced with createHashRouter, gitignore file update for .env files

--- front/src/routes/Router.js
+++ front/src/routes/Router.js
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import App from "../App.js";
 import LoginSignUpPage from "../pages/LoginSignUpPage/LoginSignUpPage.js"
 import PortfolioPage from "../pages/PortfolioPages/MyPortfolioPage.js"
@@ -6,7 +6,7 @@ import UserListPage from "../pages/UserListPage/UserListPage.js"
 import UserPortfolioPage from "../pages/PortfolioPages/UserPortfolioPage.js";
 import ErrorPage from "../pages/ErrorPage.js";
 
-export const Router = createBrowserRouter([
+export const Router = createHashRouter([
     {
         path: "/",
         element: <App />,
